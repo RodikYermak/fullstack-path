f6aecd89de5e4a0698908a821848db8e197fd7fc
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-06-18 20:29:25 -0700

    making api work

--- AI/DALLE-creating-images/index.js
+++ AI/DALLE-creating-images/index.js
@@ -1,18 +1,27 @@
-import { OpenAI } from "openai"
+import { OpenAI } from 'openai';
+const AI_API = import.meta.env.VITE_OPENAI_API_KEY;
 
-const outputImg = document.getElementById('output-img')
+const outputImg = document.getElementById('output-img');
 
 const openai = new OpenAI({
-    dangerouslyAllowBrowser: true
-})
+    dangerouslyAllowBrowser: true,
+    apiKey: AI_API,
+});
 
-document.getElementById("submit-btn").addEventListener("click", () => {
-    const prompt = document.getElementById("instruction").value
-    generateImage(prompt)
-})
+document.getElementById('submit-btn').addEventListener('click', () => {
+    const prompt = document.getElementById('instruction').value;
+    generateImage(prompt);
+});
 
 async function generateImage(prompt) {
-
-    console.log(response)
-    outputImg.innerHTML = `<img src="" alt="The Image API Failed">`
+    const response = await openai.images.generate({
+        model: 'dall-e-3',
+        prompt: prompt,
+        n: 1,
+        size: '1024x1024',
+        style: 'vivid',
+        response_format: 'b64_json',
+    });
+    console.log(response);
+    outputImg.innerHTML = `<img src="data:image/png;base64,${response.data[0].b64_json}">`;
 }
