3adeed12e33d2acd85090618835c5bbe7679687d
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-06-20 23:05:52 -0700

    create a conversational response using OpenAi

--- AI/rag-vector-databases/index.js
+++ AI/rag-vector-databases/index.js
@@ -23,21 +23,56 @@
 // main(podcasts);
 import { openai, supabase } from './config.js';
 
-const query = 'Hidden jazz';
+const query = 'Something peaceful and relaxing';
 main(query);
 
 async function main(input) {
+    const embedding = await createEmbedding(input);
+    const match = await findNearestMatch(embedding);
+    await getChatCompletion(match, input)
+}
+
+async function createEmbedding(input) {
     const embeddingResponse = await openai.embeddings.create({
         model: 'text-embedding-ada-002',
         input,
     });
+    return embeddingResponse.data[0].embedding;
+}
 
-    const embedding = embeddingResponse.data[0].embedding;
-
+async function findNearestMatch(embedding) {
     const { data } = await supabase.rpc('match_documents', {
         query_embedding: embedding,
         match_threshold: 0.5,
         match_count: 1,
     });
-    console.log(data[0].content, data[0].similarity);
+    return data[0].content;
+}
+
+const chatMessages = [
+    {
+        role: 'system',
+        content: `You are an enthusiastic podcast expert who loves recommending podcasts to people. You
+  will be given two pieces of information - some context about podcasts episodes add a question.
+  Your main job is to formulate a short answer to the question using the provided context. If you
+  are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer."
+  Please do not make up the answer.
+  `,
+    },
+];
+
+async function getChatCompletion(text, query) {
+    chatMessages.push({
+        role: 'user',
+        content: `Context: ${text} Question: ${query}`,
+    });
+
+    const response = await openai.chat.completions.create({
+        model: 'gpt-4',
+        messages: chatMessages,
+        temperature: 0.5,
+        frequency_penalty: 0.5,
+    });
+
+    console.log(response.choices[0].message.content);
 }
