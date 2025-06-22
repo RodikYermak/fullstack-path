85b41c0ea2b9fbe02746d98fa394833291975fba
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-06-21 20:44:06 -0700

    split text, get vectors, insert into Supabase

--- AI/rag-vector-databases/documents.sql
+++ AI/rag-vector-databases/documents.sql
@@ -1,4 +1,4 @@
--- -- Create a table to store your documents
+-- Create a table to store your documents
 -- create table documents (
 --   id bigserial primary key,
 --   content text, -- corresponds to the "text chunk"
@@ -6,7 +6,30 @@
 -- );
 
 -- Create a function to search for documents
-create or replace function match_documents (
+-- create or replace function match_documents (
+--   query_embedding vector(1536),
+--   match_threshold float,
+--   match_count int
+-- )
+-- returns table (
+--   id bigint,
+--   content text,
+--   similarity float
+-- )
+-- language sql stable
+-- as $$
+--   select
+--     documents.id,
+--     documents.content,
+--     1 - (documents.embedding <=> query_embedding) as similarity
+--   from documents
+--   where 1 - (documents.embedding <=> query_embedding) > match_threshold
+--   order by similarity desc
+--   limit match_count;
+-- $$;
+
+-- Create a function to search for documents
+create or replace function match_movies (
   query_embedding vector(1536),
   match_threshold float,
   match_count int
@@ -19,11 +42,11 @@ returns table (
 language sql stable
 as $$
   select
-    documents.id,
-    documents.content,
-    1 - (documents.embedding <=> query_embedding) as similarity
-  from documents
-  where 1 - (documents.embedding <=> query_embedding) > match_threshold
+    movies.id,
+    movies.content,
+    1 - (movies.embedding <=> query_embedding) as similarity
+  from movies
+  where 1 - (movies.embedding <=> query_embedding) > match_threshold
   order by similarity desc
   limit match_count;
 $$;
--- AI/rag-vector-databases/index.js
+++ AI/rag-vector-databases/index.js
@@ -21,72 +21,111 @@
 // }
 
 // main(podcasts);
+// import { openai, supabase } from './config.js';
+// import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+
+// const query = 'Something peaceful and relaxing';
+// main(query);
+
+// async function main(input) {
+//     const embedding = await createEmbedding(input);
+//     const match = await findNearestMatch(embedding);
+//     // await getChatCompletion(match, input)
+// }
+
+// async function createEmbedding(input) {
+//     const embeddingResponse = await openai.embeddings.create({
+//         model: 'text-embedding-ada-002',
+//         input,
+//     });
+//     return embeddingResponse.data[0].embedding;
+// }
+
+// async function findNearestMatch(embedding) {
+//     const { data } = await supabase.rpc('match_documents', {
+//         query_embedding: embedding,
+//         match_threshold: 0.5,
+//         match_count: 1,
+//     });
+//     return data[0].content;
+// }
+
+// const chatMessages = [
+//     {
+//         role: 'system',
+//         content: `You are an enthusiastic podcast expert who loves recommending podcasts to people. You
+//   will be given two pieces of information - some context about podcasts episodes add a question.
+//   Your main job is to formulate a short answer to the question using the provided context. If you
+//   are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer."
+//   Please do not make up the answer.
+//   `,
+//     },
+// ];
+
+// async function getChatCompletion(text, query) {
+//     chatMessages.push({
+//         role: 'user',
+//         content: `Context: ${text} Question: ${query}`,
+//     });
+
+//     const response = await openai.chat.completions.create({
+//         model: 'gpt-4',
+//         messages: chatMessages,
+//         temperature: 0.5,
+//         frequency_penalty: 0.5,
+//     });
+
+//     console.log(response.choices[0].message.content);
+// }
+
+// async function splitDocument() {
+//     const response = await fetch('podcasts.txt');
+//     const text = await response.text();
+
+//     const splitter = new RecursiveCharacterTextSplitter({
+//         chunkSize: 150,
+//         chunkOverlap: 15,
+//     });
+//     const output = await splitter.createDocuments([text]);
+//     console.log(output);
+// }
+// splitDocument();
 import { openai, supabase } from './config.js';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 
-const query = 'Something peaceful and relaxing';
-main(query);
+async function main() {}
 
-async function main(input) {
-    const embedding = await createEmbedding(input);
-    const match = await findNearestMatch(embedding);
-    // await getChatCompletion(match, input)
-}
-
-async function createEmbedding(input) {
-    const embeddingResponse = await openai.embeddings.create({
-        model: 'text-embedding-ada-002',
-        input,
-    });
-    return embeddingResponse.data[0].embedding;
-}
-
-async function findNearestMatch(embedding) {
-    const { data } = await supabase.rpc('match_documents', {
-        query_embedding: embedding,
-        match_threshold: 0.5,
-        match_count: 1,
-    });
-    return data[0].content;
-}
-
-const chatMessages = [
-    {
-        role: 'system',
-        content: `You are an enthusiastic podcast expert who loves recommending podcasts to people. You
-  will be given two pieces of information - some context about podcasts episodes add a question.
-  Your main job is to formulate a short answer to the question using the provided context. If you
-  are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer."
-  Please do not make up the answer.
-  `,
-    },
-];
-
-async function getChatCompletion(text, query) {
-    chatMessages.push({
-        role: 'user',
-        content: `Context: ${text} Question: ${query}`,
-    });
-
-    const response = await openai.chat.completions.create({
-        model: 'gpt-4',
-        messages: chatMessages,
-        temperature: 0.5,
-        frequency_penalty: 0.5,
-    });
-
-    console.log(response.choices[0].message.content);
-}
-
-async function splitDocument() {
-    const response = await fetch('podcasts.txt');
+async function splitDocument(document) {
+    const response = await fetch(`${document}`);
     const text = await response.text();
 
     const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 150,
-        chunkOverlap: 15,
+        chunkSize: 250,
+        chunkOverlap: 35,
     });
     const output = await splitter.createDocuments([text]);
     console.log(output);
+    return output;
 }
-splitDocument();
+
+async function createAndStoreEmbeddings() {
+    const chunkData = await splitDocument('movies.txt');
+
+    const data = await Promise.all(
+        chunkData.map(async (textChunk) => {
+            const embeddingResponse = await openai.embeddings.create({
+                model: 'text-embedding-ada-002',
+                input: textChunk.pageContent,
+            });
+            return {
+                content: textChunk.pageContent,
+                embedding: embeddingResponse.data[0].embedding,
+            };
+        })
+    );
+
+    await supabase.from('movies').insert(data);
+    console.log('Embedding and storing complete!');
+}
+
+createAndStoreEmbeddings();
