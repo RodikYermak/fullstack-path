e3aec154875fbb9704d3689366929759cd4620ce
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-06-26 22:16:04 -0700

    ai gateway setup

--- AI/deployment/hidden-thunder-2548/src/index.js
+++ AI/deployment/hidden-thunder-2548/src/index.js
@@ -9,21 +9,31 @@
  */
 import OpenAI from 'openai';
 
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type',
+};
+
 export default {
 	async fetch(request, env, ctx) {
 		const openai = new OpenAI({
 			apiKey: env.OPEN_API_KEY,
+			baseURL: 'https://gateway.ai.cloudflare.com/v1/f975428f0e5e4562c44ea2c44e1cba96/stock-predictions/openai',
 		});
 		try {
 			const chatCompletion = await openai.chat.completions.create({
 				model: 'gpt-3.5-turbo',
 				messages: [{ role: 'user', content: 'Should I trust stock predictions from Dodgy Dave?' }],
+				temperature: 1.1,
+				presence_penalty: 0,
+				frequency_penalty: 0,
 			});
 			const response = chatCompletion.choices[0].message;
 
-			return new Response(JSON.stringify(response));
+			return new Response(JSON.stringify(response), { headers: corsHeaders });
 		} catch (e) {
-			return new Response(e);
+			return new Response(e, { headers: corsHeaders });
 		}
 	},
 };
