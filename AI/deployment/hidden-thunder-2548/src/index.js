33d13ffea5469f0a62fe95b1050249d7d5f12940
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-06-26 21:31:42 -0700

    storing api key

--- AI/deployment/hidden-thunder-2548/src/index.js
+++ AI/deployment/hidden-thunder-2548/src/index.js
@@ -7,9 +7,29 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import OpenAI from 'openai';
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const openai = new OpenAI({
+			apiKey: env.OPEN_API_KEY,
+		});
+		try {
+			const chatCompletion = await openai.chat.completions.create({
+				model: 'gpt-3.5-turbo',
+				messages: [{ role: 'user', content: 'Should I trust stock predictions from Dodgy Dave?' }],
+			});
+			const response = chatCompletion.choices[0].message;
+
+			return new Response(JSON.stringify(response));
+		} catch (e) {
+			return new Response(e);
+		}
 	},
 };
+
+// Cloudflare Worker project
+// 01 Install OpenAI API library in worker
+// 02 Instantiate OpenAI API client
+// 03 Save API key to your environment
+// 04 Make an OpenAI request
