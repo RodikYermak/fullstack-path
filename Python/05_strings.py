aa022ec91fc02b083bc4f55d47b0a7438306d2ce
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-07-16 11:23:21 -0700

    strings

---
+++ Python/05_strings.py
@@ -0,0 +1,12 @@
+#.   0123456789
+msg='welcome to Python 101: Strings'
+extract = f"{msg[18]} {msg[0:7].title()} {msg[-5:-1].title()} {msg[8:10].title()} Tyler"
+msg2="""Dear Terry,,
+You must cut down the mightiest 
+tree in the forest with…
+a herring! <3"""
+name = 'TERRY'
+print(msg)
+print(msg2)
+print(extract)
+print(name.capitalize())
