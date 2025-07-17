fa85f750fdbc8a66d90f4355f901fd7b719e5661
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-07-17 15:11:22 -0700

    commit

---
+++ Python/15_loops.py
@@ -0,0 +1,12 @@
+names = ['john ClEEse','Eric IDLE','michael']
+names1 = ['graHam chapman', 'TERRY', 'terry jones']
+msg = 'You are invited to the party on saturday.'
+#names.extend(names1)
+names += names1
+for index in range(2):
+    names.append(input('Enter a new name: '))
+
+for name in names:
+    #msg1 = f'{name.title()}! {msg}'
+    msg1 = name.title() + '! ' + msg
+    print(msg1)
