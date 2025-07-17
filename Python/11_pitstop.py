ef0630cac8188513c6833f61d363e5ac9b4f5bf7
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-07-17 10:45:47 -0700

    commit

--- Python/11_pitstop.py
+++ Python/11_pitstop.py
@@ -13,3 +13,19 @@
 # - Total pit stop time in seconds
 # - Percentage of race time spent in pits
 # - A final message if pit time > 5% of the race: "You need a new pit crew. 🛠️"
+time = float(input('What is the total race time: '))
+stops = int(input('How many pit stops were made: '))
+duration = int(input('The average pit stop suration: '))
+
+total_pitstop_time = stops * duration
+pits_percentage = round(total_pitstop_time / time * 100, 2)
+
+print(time)
+print(stops)
+print(duration)
+
+print(f'Total pit stop time in {total_pitstop_time} seconds')
+print(f'Percentage of race time spent in pits: {pits_percentage}')
+
+if pits_percentage > 5:
+    print('You need a new pit crew.')
