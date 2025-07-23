5a08745a2f5786d1f0d0989a347ce89bd25ef6ce
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-07-23 14:08:29 -0700

    commit

--- Python/code_wars.py
+++ Python/code_wars.py
@@ -1,17 +1,2 @@
-def high(x):
-    print('test')
-
-# Manual test cases using built-in assert
-def run_tests():
-    assert high('man i need a taxi up to ubud') == 'taxi'
-    assert high('what time are we climbing up the volcano') == 'volcano'
-    assert high('take me to semynak') == 'semynak'
-    assert high('aa b') == 'aa'
-    assert high('b aa') == 'b'
-    assert high('bb d') == 'bb'
-    assert high('d bb') == 'd'
-    assert high('aaa b') == 'aaa'
-    print("All tests passed.")
-
-# Run the tests
-# run_tests()
+if 5 > 2:
+    print('five')
