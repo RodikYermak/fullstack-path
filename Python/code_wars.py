2fa077783b42457d767e970e67e6bc4da685dd24
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-07-24 16:47:53 -0700

    commit

--- Python/code_wars.py
+++ Python/code_wars.py
@@ -1,21 +1,4 @@
-import random
+dict1 = {"name": "Rodion", "age": 36}
 
-if 5 > 2:
-    print('five')
-
-list = [1, 2, 3]
-
-# print(list)
-
-x = 35.99
-
-print(int(x))
-
-print(random.randrange(1, 10))
-
-text_center = 'Returns a centered string'
-
-print(text_center.center(60))
-print(text_center.count('e'))
-print(text_center.isdecimal())
-# text_center
+for key, value in dict1.items():
+    print(key, value)
