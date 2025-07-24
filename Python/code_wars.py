9ef15b6660f311d89f2c7b3c8d2da473dcafaf7b
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-07-23 23:24:34 -0700

    commit

--- Python/code_wars.py
+++ Python/code_wars.py
@@ -12,3 +12,10 @@ x = 35.99
 print(int(x))
 
 print(random.randrange(1, 10))
+
+text_center = 'Returns a centered string'
+
+print(text_center.center(60))
+print(text_center.count('e'))
+print(text_center.isdecimal())
+# text_center
