5f6b1425c0fb03b94e5b3665a92ff852a328ea4a
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-04 08:14:34 -0700

    commit

--- Python/31_bst_tree.py
+++ Python/31_bst_tree.py
@@ -1,1 +1,11 @@
-print('test')
+
+# def contains(self, value):
+#     temp = self.root
+#     while temp is not None:
+#         if value < temp.value:
+#             temp = temp.left
+#         elif value > temp.value:
+#             temp = temp.right
+#         else:
+#             return True
+#     return False
