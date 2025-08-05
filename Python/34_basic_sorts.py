a15027e85add9090f1be45dd60111759b85d9352
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-05 11:38:45 -0700

    commit

---
+++ Python/34_basic_sorts.py
@@ -0,0 +1,57 @@
+def bubble_sort(my_list):
+    for i in range(len(my_list) - 1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = temp
+    return my_list
+
+print(bubble_sort([4,2,6,5,1,3]))
+
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        min_index = i
+        for j in range(i+1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
+    return my_list
+
+print(selection_sort([1,2,5,6,4,3]))
+
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i-1
+        while temp < my_list[j] and j > -1:
+            my_list[j+1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+print(insertion_sort([1,2,5,6,4,3]))
+
+def merge(list1, list2):
+    combined = []
+    i = 0
+    j = 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+    return combined
+
+print(merge([1, 2, 7, 8], [3,4,5,6]))
