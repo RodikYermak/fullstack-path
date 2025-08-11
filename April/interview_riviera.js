d6a0f0cfd39f78ab62e61836fead41e01ec71b20
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-11 16:15:01 -0700

    commit

---
+++ April/interview_riviera.js
@@ -0,0 +1,39 @@
+// console.log('hi');
+
+// class
+// node
+// value
+// next
+
+// List class 
+// constructor will have a node assigned with vale node from node class
+// next should Null
+
+class Node {
+    constructor(next, value){
+        this.next = next;
+        this.value = value
+    }
+}
+
+class List {
+    constructor(){
+    }
+    // head is a first node
+    // tail is a last node
+
+    // counter 
+    // add or append new node 
+    // insert
+    // append
+    append(value){
+        // find a tail
+        // create a temp variable to store the old last node in it
+        // move a tail one elemenent and assign that to new noode
+        //  // this.tail = temp;
+        
+    }
+}
+
+const link1 = List(10)
+
