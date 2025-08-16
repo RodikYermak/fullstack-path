2ccfabe2386ff49dfb3b188ef813f82dd5c60728
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-16 13:02:52 -0700

    commit

--- SQL/query.sql
+++ SQL/query.sql
@@ -174,12 +174,43 @@
 	Order by the condition in descending order
 */
 
-SELECT brand, model, condition, 
-  CASE
-    WHEN condition >= 4 THEN 'Excellent'
-    WHEN condition >= 3 THEN 'Fair'
-    WHEN condition >= 1 THEN 'Poor'
-    ELSE 'Wrecked'
-  END AS condition_label
-  FROM cars
-  ORDER BY condition DESC;
+-- SELECT brand, model, condition, 
+--   CASE
+--     WHEN condition >= 4 THEN 'Excellent'
+--     WHEN condition >= 3 THEN 'Fair'
+--     WHEN condition >= 1 THEN 'Poor'
+--     ELSE 'Wrecked'
+--   END AS condition_label
+--   FROM cars
+--   ORDER BY condition DESC;
+
+/*
+	Rodney is assigning bonuses for his staff on the following terms:
+		- Salespeople who have made more than $100,000 in sales receive $10,000
+		- Salespeople who have made more than $75,000 in sales receive $5,000
+		- Every other staff member gets a bonus of $1,000
+	
+	Select from the sold_cars table, joined with staff where
+		sold_cars(seller) = staff(id)
+	Show all staff, even if they have made no sales
+	
+	Select:
+		* staff(name)
+		* staff(role)
+		* staff(dealership_id)
+		* the sum of the staff member's sales - alias as total_sales
+	
+	Use CASE to select the correct value, as defined above, aliased as bonus
+
+	Use GROUP BY to format the output, and ORDER BY bonus then dealership_id
+*/
+SELECT S.name, S.role, S.dealership_id, SUM(SC.sold_price) AS total_sales,
+CASE
+    WHEN SUM(SC.sold_price) >= 100000 THEN 10000
+    WHEN SUM(SC.sold_price) >= 75000 THEN 5000
+    ELSE 1000
+END AS bonus
+FROM sold_cars SC
+RIGHT JOIN staff S ON SC.seller = S.id
+GROUP BY (S.name, S.role, S.dealership_id)
+ORDER BY bonus, dealership_id;
