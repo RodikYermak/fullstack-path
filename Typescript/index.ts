9a4592d2e66a1109ec4b9ea2b430b7acd7d4313b
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-21 11:52:43 -0700

    commit

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -302,14 +302,10 @@
  */
 
 type Pizza = {
+    id: number;
     name: string;
     price: number;
 };
-
-/**
- * Challenge: using literal types and unions, update the Order status so that
- * it can only ever be "ordered" or "completed"
- */
 
 type Order = {
     id: number;
@@ -317,22 +313,37 @@ type Order = {
     status: 'ordered' | 'completed';
 };
 
-const menu = [
-    { name: 'Margherita', price: 8 },
-    { name: 'Pepperoni', price: 10 },
-    { name: 'Hawaiian', price: 10 },
-    { name: 'Veggie', price: 9 },
-];
-
 let cashInRegister = 100;
 let nextOrderId = 1;
-const orderHistory: Order[] = [];
+let nextPizzaId = 1;
 
-function addNewPizza(pizzaObj: Pizza) {
-    menu.push(pizzaObj);
+const menu: Pizza[] = [
+    { id: nextPizzaId++, name: 'Margherita', price: 8 },
+    { id: nextPizzaId++, name: 'Pepperoni', price: 10 },
+    { id: nextPizzaId++, name: 'Hawaiian', price: 10 },
+    { id: nextPizzaId++, name: 'Veggie', price: 9 },
+];
+
+const orderQueue: Order[] = [];
+
+/**
+ * Challenge:
+ * Fix the addNewPizza function using the Omit utility type. This might
+ * require more than just changing the "Pizza" typed `pizzaObj` parameter.
+ * Return the new pizza object (with the id added) from the function.
+ */
+
+function addNewPizza(pizzaObj: Readonly<Omit<Pizza, 'id'>>): Pizza {
+    const pizza: Pizza = { id: nextPizzaId++, ...pizzaObj };
+    menu.push(pizza);
+    return pizza;
 }
 
-function placeOrder(pizzaName: string) {
+addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
+addNewPizza({ name: 'BBQ Chicken', price: 12 });
+addNewPizza({ name: 'Spicy Sausage', price: 11 });
+
+function placeOrder(pizzaName: string): Order | undefined {
     const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
     if (!selectedPizza) {
         console.error(`${pizzaName} does not exist in the menu`);
@@ -340,12 +351,12 @@ function placeOrder(pizzaName: string) {
     }
     cashInRegister += selectedPizza.price;
     const newOrder: Order = { id: nextOrderId++, pizza: selectedPizza, status: 'ordered' };
-    orderHistory.push(newOrder);
+    orderQueue.push(newOrder);
     return newOrder;
 }
 
-function completeOrder(orderId: number) {
-    const order = orderHistory.find((order) => order.id === orderId);
+function completeOrder(orderId: number): Order | undefined {
+    const order = orderQueue.find((order) => order.id === orderId);
     if (!order) {
         console.error(`${orderId} was not found in the orderQueue`);
         return;
@@ -354,13 +365,22 @@ function completeOrder(orderId: number) {
     return order;
 }
 
-addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
-addNewPizza({ name: 'BBQ Chicken', price: 12 });
-addNewPizza({ name: 'Spicy Sausage', price: 11 });
+export function getPizzaDetail(identifier: string | number): Pizza | undefined {
+    if (typeof identifier === 'string') {
+        return menu.find((pizza) => pizza.name.toLowerCase() === identifier.toLowerCase());
+    } else if (typeof identifier === 'number') {
+        return menu.find((pizza) => pizza.id === identifier);
+    } else {
+        throw new TypeError('Parameter `identifier` must be either a string or a number');
+    }
+}
 
-placeOrder('Chicken Bacon Ranch');
-completeOrder(1);
+// placeOrder("Chicken Bacon Ranch")
+// placeOrder("Pepperoni")
+// completeOrder(1)
+// placeOrder("Veggie")
+// completeOrder(2)
 
 console.log('Menu:', menu);
-console.log('Cash in register:', cashInRegister);
-console.log('Order queue:', orderHistory);
+// console.log("Cash in register:", cashInRegister)
+// console.log("Order queue:", orderQueue)
