239b1fcc0ed0e85aa34cad87831235488559adc1
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-21 07:38:29 -0700

    commit

--- Typescript/index.js
+++ Typescript/index.js
@@ -5,38 +5,35 @@ const menu = [
     { name: 'Veggie', price: 9 },
 ];
 
-let cashInRegister = 100;
+const cashInRegister = 100;
+const nextOrderId = 1;
 const orderQueue = [];
 
-/**
- * Challenge: Add a utility function "addNewPizza" that takes a pizza object
- * and adds it to the menu.
- */
-
 function addNewPizza(pizzaObj) {
-    return menu.push(pizzaObj);
+    menu.push(pizzaObj);
 }
-
-/**
- * Write another utility function, placeOrder, that takes a pizza name parameter and:
- * 1. finds that pizza object in the menu,
- * 2. adds the income to the cashInRegister,
- * 3. pushes a new "order object" to the orderQueue
- *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
- * 4. returns the new order object (just in case we need it later)
- */
 
 function placeOrder(pizzaName) {
-    const pizza = menu.find((p) => p.name === pizzaName);
-    if (!pizza) {
-        throw new Error(`"${pizzaName}" is not on the menu`);
-    }
+    const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+    cashInRegister += selectedPizza.price;
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: 'ordered' };
+    orderQueue.push(newOrder);
+    return newOrder;
+}
 
-    cashInRegister += pizza.price;
-
-    const order = { pizza, status: 'ordered' };
-
-    orderQueue.push(order);
-
+function completeOrder(orderId) {
+    const order = orderQueue.find((order) => order.id === orderId);
+    order.status = 'completed';
     return order;
 }
+
+addNewPizza({ name: 'Chicken Bacon Ranch', cost: 12 });
+addNewPizza({ name: 'BBQ Chicken', cost: 12 });
+addNewPizza({ name: 'Spicy Sausage', cost: 11 });
+
+placeOrder('Chicken Bacon Ranch');
+completeOrder('1');
+
+console.log('Menu:', menu);
+console.log('Cash in register:', cashInRegister);
+console.log('Order queue:', orderQueue);
