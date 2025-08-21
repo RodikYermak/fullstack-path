9fa45e25053aa645f57c27d23d418fbbd2de4797
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-21 10:04:53 -0700

    commit

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -96,20 +96,270 @@
 
 // Challenge: finish the object type definition
 
-type Person = {
+// type Person = {
+//     name: string;
+//     age: number;
+//     isStudent: boolean;
+// };
+
+// let person1 = {
+//     name: 'Joe',
+//     age: 42,
+//     isStudent: true,
+// };
+
+// let person2 = {
+//     name: 'Jill',
+//     age: 66,
+//     isStudent: false,
+// };
+
+/**
+ * Challenge: Create a Pizza object type. It should include a `name`
+ * and a `price` property.
+ */
+// type Pizza = {
+//     name: string;
+//     price: number;
+// };
+
+// const menu: Pizza[] = [
+//     { name: 'Margherita', price: 8 },
+//     { name: 'Pepperoni', price: 10 },
+//     { name: 'Hawaiian', price: 10 },
+//     { name: 'Veggie', price: 9 },
+// ];
+
+// let cashInRegister = 100;
+// let nextOrderId = 1;
+// const orderQueue = [];
+
+// function addNewPizza(pizzaObj: Pizza) {
+//     menu.push(pizzaObj);
+// }
+
+// function placeOrder(pizzaName) {
+//     const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+//     if (!selectedPizza) {
+//         console.error(`${pizzaName} does not exist in the menu`);
+//         return;
+//     }
+//     cashInRegister += selectedPizza.price;
+//     const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: 'ordered' };
+//     orderQueue.push(newOrder);
+//     return newOrder;
+// }
+
+// function completeOrder(orderId: number) {
+//     const order = orderQueue.find((order) => order.id === orderId);
+//     order.status = 'completed';
+//     return order;
+// }
+
+// addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
+// addNewPizza({ name: 'BBQ Chicken', price: 12 });
+// addNewPizza({ name: 'Spicy Sausage', price: 11 });
+
+// placeOrder('Chicken Bacon Ranch');
+// completeOrder(1);
+
+// console.log('Menu:', menu);
+// console.log('Cash in register:', cashInRegister);
+// console.log('Order queue:', orderQueue);
+
+// Challenge: try to figure out how to move the nested address object type
+// into a separate type definition. When done correctly, there should be no more
+// red errors in the editor
+
+// type Address = {
+//     street: string;
+//     city: string;
+//     country: string;
+// };
+
+// type Person = {
+//     name: string;
+//     age: number;
+//     isStudent: boolean;
+//     address: Address;
+// };
+
+// let person1: Person = {
+//     name: 'Joe',
+//     age: 42,
+//     isStudent: true,
+//     address: {
+//         street: '123 Main',
+//         city: 'Anytown',
+//         country: 'USA',
+//     },
+// };
+
+// let person2: Person = {
+//     name: 'Jill',
+//     age: 66,
+//     isStudent: false,
+//     address: {
+//         street: '123 Main',
+//         city: 'Anytown',
+//         country: 'USA',
+//     },
+// };
+
+// type Pizza = {
+//     name: string;
+//     price: number;
+// };
+
+/**
+ * Challenge: Add an Order type. It should have `id`, `pizza`, and `status` properties.
+ * Look through the code if you need a reminder as to what data types those should be.
+ */
+// type Order = {
+//     id: number;
+//     pizza: {
+//         name: string;
+//         price: number;
+//     };
+//     status: string;
+// };
+
+// const menu = [
+//     { name: 'Margherita', price: 8 },
+//     { name: 'Pepperoni', price: 10 },
+//     { name: 'Hawaiian', price: 10 },
+//     { name: 'Veggie', price: 9 },
+// ];
+
+// let cashInRegister = 100;
+// let nextOrderId = 1;
+// const orderQueue: Order[] = [];
+
+// function addNewPizza(pizzaObj: Pizza) {
+//     menu.push(pizzaObj);
+// }
+
+// function placeOrder(pizzaName: string) {
+//     const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+//     if (!selectedPizza) {
+//         console.error(`${pizzaName} does not exist in the menu`);
+//         return;
+//     }
+//     cashInRegister += selectedPizza.price;
+//     const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: 'ordered' };
+//     orderQueue.push(newOrder);
+//     return newOrder;
+// }
+
+// function completeOrder(orderId: number) {
+//     const order = orderQueue.find((order) => order.id === orderId);
+//     order.status = 'completed';
+//     return order;
+// }
+
+// addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
+// addNewPizza({ name: 'BBQ Chicken', price: 12 });
+// addNewPizza({ name: 'Spicy Sausage', price: 11 });
+
+// placeOrder('Chicken Bacon Ranch');
+// completeOrder(1);
+
+// console.log('Menu:', menu);
+// console.log('Cash in register:', cashInRegister);
+// console.log('Order queue:', orderQueue);
+
+// type Person = {
+//     name: string;
+//     age: number;
+//     isStudent: boolean;
+// };
+
+// let person1: Person = {
+//     name: 'Joe',
+//     age: 42,
+//     isStudent: true,
+// };
+
+// let person2: Person = {
+//     name: 'Jill',
+//     age: 66,
+//     isStudent: false,
+// };
+
+// /**
+//  * Challenge: create an array of people objects and
+//  * manually type it as an array of Person types
+//  */
+
+// let people: Person[] = [
+//     { name: 'Joe', age: 42, isStudent: true },
+//     { name: 'Jill', age: 66, isStudent: false },
+//     { name: 'Sam', age: 25, isStudent: true },
+// ];
+
+/**
+ * Challenge: Fix the TS warnings about orderQueue!
+ */
+
+type Pizza = {
     name: string;
-    age: number;
-    isStudent: boolean;
+    price: number;
 };
 
-let person1 = {
-    name: 'Joe',
-    age: 42,
-    isStudent: true,
+type Order = {
+    id: number;
+    pizza: Pizza;
+    status: string;
 };
 
-let person2 = {
-    name: 'Jill',
-    age: 66,
-    isStudent: false,
-};
+const menu = [
+    { name: 'Margherita', price: 8 },
+    { name: 'Pepperoni', price: 10 },
+    { name: 'Hawaiian', price: 10 },
+    { name: 'Veggie', price: 9 },
+];
+
+let cashInRegister = 100;
+let nextOrderId = 1;
+const orderQueue: Order[] = [];
+
+function addNewPizza(pizzaObj: Pizza) {
+    menu.push(pizzaObj);
+}
+
+function placeOrder(pizzaName: string) {
+    const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+    if (!selectedPizza) {
+        console.error(`${pizzaName} does not exist in the menu`);
+        return;
+    }
+    cashInRegister += selectedPizza.price;
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: 'ordered' };
+    orderQueue.push(newOrder);
+    return newOrder;
+}
+
+/**
+ * Challenge: Fix the warning below by handling the "sad path" scenario!
+ */
+
+function completeOrder(orderId: number) {
+    const order = orderQueue.find((order) => order.id === orderId);
+    if (!order) {
+        console.error(`${order} does noot exist in the order`);
+        return;
+    }
+    order.status = 'completed';
+    return order;
+}
+
+addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
+addNewPizza({ name: 'BBQ Chicken', price: 12 });
+addNewPizza({ name: 'Spicy Sausage', price: 11 });
+
+placeOrder('Chicken Bacon Ranch');
+completeOrder(1);
+
+console.log('Menu:', menu);
+console.log('Cash in register:', cashInRegister);
+console.log('Order queue:', orderQueue);
