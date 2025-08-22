01272f4ee451da35dc6f3d0033bb4d6bea861a70
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-22 12:03:44 -0700

    commit

--- Typescript/React/src/components/WordLetters.tsx
+++ Typescript/React/src/components/WordLetters.tsx
@@ -1,19 +1,26 @@
-import { clsx } from "clsx"
+import { clsx } from 'clsx';
+import type { JSX } from 'react';
 
-export default function WordLetters({ currentWord, guessedLetters, isGameLost }) {
+type WordLettersProps = {
+    currentWord: string;
+    guessedLetters: string[];
+    isGameLost: boolean;
+};
+
+export default function WordLetters({ currentWord, guessedLetters, isGameLost }:WordLettersProps):JSX.Element {
     return (
         <section className="word">
-            {currentWord.split("").map((letter, index) => {
-                const shouldRevealLetter = isGameLost || guessedLetters.includes(letter)
-                const letterClassName = clsx(
-                    isGameLost && !guessedLetters.includes(letter) && "missed-letter"
-                )
+            {currentWord.split('').map((letter:string, index:number):JSX.Element => {
+                const shouldRevealLetter: boolean | string = isGameLost || guessedLetters.includes(letter);
+                const letterClassName: boolean | string = clsx(
+                    isGameLost && !guessedLetters.includes(letter) && 'missed-letter'
+                );
                 return (
                     <span key={index} className={letterClassName}>
-                        {shouldRevealLetter ? letter.toUpperCase() : ""}
+                        {shouldRevealLetter ? letter.toUpperCase() : ''}
                     </span>
-                )
+                );
             })}
         </section>
-    )
+    );
 }
