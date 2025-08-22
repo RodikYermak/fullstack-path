e352d55a4fec30dd56825a1abceda659648004eb
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-22 11:19:49 -0700

    commit

--- Typescript/React/src/components/LanguageChips.tsx
+++ Typescript/React/src/components/LanguageChips.tsx
@@ -1,23 +1,34 @@
-import { clsx } from "clsx"
+import { clsx } from 'clsx';
+import type { JSX } from 'react';
 
-export default function LanguageChips({ languages, wrongGuessCount }) {
+type Language = {
+    name: string;
+    backgroundColor: string;
+    color: string;
+};
+
+type LanguageChipsProps = {
+    languages: Language[];
+    wrongGuessCount: number;
+};
+
+export default function LanguageChips({
+    languages,
+    wrongGuessCount,
+}: LanguageChipsProps): JSX.Element {
     const languageElements = languages.map((lang, index) => {
-        const isLanguageLost = index < wrongGuessCount
+        const isLanguageLost = index < wrongGuessCount;
         const styles = {
             backgroundColor: lang.backgroundColor,
-            color: lang.color
-        }
-        const className = clsx("chip", isLanguageLost && "lost")
+            color: lang.color,
+        };
+        const className = clsx('chip', isLanguageLost && 'lost');
         return (
-            <span
-                className={className}
-                style={styles}
-                key={lang.name}
-            >
-        {lang.name}
-      </span>
-        )
-    })
+            <span className={className} style={styles} key={lang.name}>
+                {lang.name}
+            </span>
+        );
+    });
 
-    return <section className="language-chips">{languageElements}</section>
+    return <section className="language-chips">{languageElements}</section>;
 }
