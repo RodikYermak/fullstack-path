818f289999f394f92dad91b4d3f641ac92e796c1
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-25 09:51:01 -0700

    commit

--- Node/server.js
+++ Node/server.js
@@ -1,5 +1,6 @@
 import http from 'node:http';
 import { getDataFromDB } from './database/db.js';
+import { sendJSONResponse } from './utils/sendJSONResponse.js';
 
 const PORT = 8000;
 
@@ -7,9 +8,7 @@ const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB();
 
     if (req.url === '/api' && req.method === 'GET') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(destinations));
+        sendJSONResponse(res, 200, destinations);
     } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
         const parts = req.url.split('/');
         const continentName = parts[parts.length - 1];
@@ -18,14 +17,10 @@ const server = http.createServer(async (req, res) => {
             (destination) => destination.continent.toLowerCase() === continentName.toLowerCase()
         );
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(filteredData));
+        sendJSONResponse(res, 200, filteredData);
     } else {
         const errorBody = { error: 'not found', message: 'The requested route does not exist' };
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(errorBody));
+        sendJSONResponse(res, 404, errorBody);
     }
 });
 
