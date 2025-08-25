08cd5f4a19847017c621d608c69de2a60cd8c1bc
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-25 08:47:09 -0700

    commit

--- Node/server.js
+++ Node/server.js
@@ -7,7 +7,14 @@ const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB();
 
     if (req.url === '/api' && req.method === 'GET') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(destinations));
+    } else {
+        const errorBody = { error: 'not found', message: 'The requested route does not exist' };
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(errorBody));
     }
 });
 
