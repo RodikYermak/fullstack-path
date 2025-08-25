adee9fdeab793ba9da192cb12162125b90cd5335
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-25 07:34:55 -0700

    commit

--- Node/server.js
+++ Node/server.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import http from 'node:http';
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!');
+});
+
+server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
