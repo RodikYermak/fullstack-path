10c92072abde94c4fa8500aa5c4d999ec89bb8a9
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2025-08-28 09:50:11 -0700

    commit

--- Fullstack_Node/server.js
+++ Fullstack_Node/server.js
@@ -1,25 +1,12 @@
 import http from 'node:http';
-import path from 'node:path';
-import serveStatic from 'utils/serveStatic.js';
-import fs from 'node:fs/promises';
+import { serveStatic } from './utils/serveStatic.js';
 
 const PORT = 8000;
 
 const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
-    const absPathToResource = path.join(__dirname, 'public', 'index.html');
-    const relPathToResource = path.join('public', 'index.html');
-    console.log('absolute', absPathToResource);
-    console.log('relative', relPathToResource);
-
-    serveStatic(__dirname);
-    const content = await fs.readFile(absPathToResource);
-    console.log(content);
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(content);
+    await serveStatic(req, res, __dirname);
 });
 
-server.listen(PORT, () => console.log('Connected'));
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
